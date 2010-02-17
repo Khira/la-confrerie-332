43088d91ca9ef066b9cb7e9c983d3b19c1793cd1
Khira <ileanah77@gmail.com>
2010-02-17 20:14:50 +0100

    [K:9] Oublie de la requête SQL pour la commande .itemnumber.
    Signed-off-by: Khira <ileanah77@gmail.com>

---
+++ [K]_SQL/K9_01_mangos_mangos_string.sql
@@ -0,0 +1,2 @@
+﻿insert into `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('11510','%s possède %u exemplaire(s) de l\'objet %u.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) values('11511','%s ne possède aucun exemplaire de l\'objet %u.',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
